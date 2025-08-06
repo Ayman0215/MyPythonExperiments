b153258fdbb808e3fb2986e64ec2092cc854be05
Ayman Mohammed <aymanm0215@gmail.com>
2025-08-06 15:12:11 -0700

    Added File - [Chapter1_Basics.py]

---
+++ Chapter1_Basics.py
@@ -0,0 +1,46 @@
+""" name = input("Enter Name: ")
+print("Entered Name: " + name) """
+
+#2. Write a Program that takes two numbers as inputs , adds them and prints their sum
+
+""" number = input("Pick one numbers between 1 and 10: ")
+print(type(number))
+print(number)
+number1 = int(number)
+number2 = input("Now pick a number between your number and a number greater than it: ")
+print(number2)
+number2 = int(number2)
+sum = number1 + number2
+sum = str(sum)
+print("Here is that your first number and your second combined is " + sum)  """
+
+# Create a program that asks user for their age and prints whether they are an adult or a minor.
+""" age = input("What is your age :")
+print(age)
+
+if int(age) >= 18 :
+    print("You are an Adult")
+else:
+    print("You are a minor")
+ """
+""" #Create a program that takes a number as input and prints the square of that number
+number = float(input("Pick any number: "))
+print(number)
+number2 = number ** 2
+number2 = str(number2)
+print("The square of that number is " + number2)
+ """
+
+""" x= 5.3
+print(type(x))
+x= 5
+print(type(x))
+x= '5.3'
+print(type(x)) """
+
+PI = 3.1415
+x = 299792456
+GRAVITY = 9.81
+
+AreaOfCircle = 2*PI*x
+print(AreaOfCircle)
