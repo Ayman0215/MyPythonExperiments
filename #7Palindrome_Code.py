c450059c37b1464229a830695fb59cf23768f505
Ayman0215 <aymanm0215@gmail.com>
2025-12-27 17:34:32 -0800

    Add files via upload

---
+++ #7Palindrome_Code.py
@@ -0,0 +1,43 @@
+#----------------------------------------------------------------#
+import unittest
+#----------------------------------------------------------------#
+def is_palindrome(string1):
+    
+    if string1 == "":
+        return False
+
+    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    has_letter = False
+
+    for char in string1:
+        if char in letters:
+            has_letter = True
+
+    if not has_letter:
+        return False
+
+    if string1 == string1[::-1]:
+        return True
+    else:
+        return False
+#----------------------------------------------------------------#
+class AnagramCode(unittest.TestCase):
+
+    def test_palindromeWithLetters(self):
+        self.assertEqual(is_palindrome("racecar"), True)
+
+    def test_notPalindromeWithLetters(self):
+        self.assertEqual(is_palindrome("apple"), False)
+
+    def test_palindromeWithPunctuation(self):
+        self.assertEqual(is_palindrome("!.??.!"), False)
+
+    def test_notPalindromeWithPunctuation(self):
+        self.assertEqual(is_palindrome("/@\\?"), False)
+
+    def test_emptyString(self):
+        self.assertEqual(is_palindrome(""), False)
+#----------------------------------------------------------------#
+if __name__ == "__main__":
+    unittest.main()
+#----------------------------------------------------------------#
