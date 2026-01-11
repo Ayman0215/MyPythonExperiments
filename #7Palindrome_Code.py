57b841f46566e9ce7453cb150011f5424842bb1c
Ayman Mohammed <aymanm0215@gmail.com>
2026-01-10 21:34:47 -0800

    updating local main

--- #7Palindrome_Code.py
+++ #7Palindrome_Code.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #----------------------------------------------------------------#
 import unittest
 #----------------------------------------------------------------#
@@ -40,4 +41,35 @@ class AnagramCode(unittest.TestCase):
 #----------------------------------------------------------------#
 if __name__ == "__main__":
     unittest.main()
+=======
+#----------------------------------------------------------------#
+import unittest
+#----------------------------------------------------------------#
+def is_palindrome(string1):
+
+    if string1 == string1[::-1]:
+        return True
+    else:
+        return False
+#----------------------------------------------------------------#
+class AnagramCode(unittest.TestCase):
+
+    def test_palindromeWithLetters(self):
+        self.assertEqual(is_palindrome("racecar"), True)
+
+    def test_notPalindromeWithLetters(self):
+        self.assertEqual(is_palindrome("apple"), False)
+
+    def test_palindromeWithPunctuation(self):
+        self.assertEqual(is_palindrome("!.??.!"), True)
+
+    def test_notPalindromeWithPunctuation(self):
+        self.assertEqual(is_palindrome("/@\\?"), False)
+
+    def test_emptyString(self):
+        self.assertEqual(is_palindrome(""), True)
+#----------------------------------------------------------------#
+if __name__ == "__main__":
+    unittest.main()
+>>>>>>> Stashed changes
 #----------------------------------------------------------------#
