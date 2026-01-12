979cbd5459b85266c7a84b79e7882a279f02fc88
Ayman0215 <aymanm0215@gmail.com>
2026-01-11 16:40:27 -0800

    Add files via upload

---
+++ #11RepeatitionInAList.py
@@ -0,0 +1,5 @@
+nums = list(map(int, input("Enter list of numbers: ").split()))
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if nums[i] == nums[j]:
+            print(f"Duplicate: {nums[i]}")
