a58718a215dbdb4df1c2909b0353f04d0c91c1e5
Ayman Mohammed <aymanm0215@gmail.com>
2026-04-22 18:47:25 -0700

    Modified Files With New Program

--- printUpperStrings.py
+++ printUpperStrings.py
@@ -1,6 +1,11 @@
 #----------------------------------------------------------------#
 def printUpperStrings(list1):
-    print(list(map(str.upper, list1)))
+    for i in range(len(list1)):
+        result = ""
+        for j in range(len(list1[i])):
+            result += list1[i][j].upper()
+        list1[i] = result
+    print(list1)
 #----------------------------------------------------------------#
 lst1 = list(map(str, input("Enter list of strings: ").split()))
 printUpperStrings(lst1)
